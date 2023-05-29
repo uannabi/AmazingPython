9647899dc54da9451f284a41c0a4bd442f4080a4
uannabi <zahid_sc@hotmail.fr>
2023-05-29 23:18:09 +0100

    asb

---
+++ absVsProto.py
@@ -0,0 +1,30 @@
+
+from abc import ABC, abstractmethod
+
+class Shape(ABC):
+    @abstractmethod
+    def area(self):
+        pass
+
+class Rectangle(Shape):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+    
+    def area(self):
+        return self.width * self.height
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+    
+    def area(self):
+        return 3.14 * (self.radius ** 2)
+
+# Usage
+rectangle = Rectangle(5, 3)
+circle = Circle(2)
+
+print(rectangle.area())  # Output: 15
+print(circle.area())     # Output: 12.56
+
