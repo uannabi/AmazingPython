49aa0ef9a6c356bb0939e3eef07dbdf948948027
uannabi <zahid_sc@hotmail.fr>
2023-05-30 23:50:24 +0100

    under score

---
+++ underscore.py
@@ -0,0 +1,9 @@
+
+numbers = [1, 2, 3, 4, 5]
+
+# Ignoring values using underscore placeholder
+for _ in numbers:
+    print('numbers')
+    # Perform some operation without using the value
+    pass
+
